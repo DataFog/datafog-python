394dd5067c80ad6ade7c96ade2c8ac960edd2ed9
Sid Mohan <sidmohan@Sids-MacBook-Pro.local>
2024-02-16 13:15:37 -0800

    cleaned up gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,3 @@
-# Iw want to create a script that basically runs: setup.py sdist bdist_wheel and then deploys the package to pypi
-# I want to be able to run this script from the root of the project
-# when I run the script, I want to be able to pass in the version number as an argument
-
-# I want to be able to run the script like this:
-# ./deploy.sh 0.0.1
-
-# I want to be able to run the script without any arguments and it should prompt me for the version number
-
-# I want to be able to run the script with the -h or --help flag and it should print out a help message
 
 
 python setup.py sdist bdist_wheel
